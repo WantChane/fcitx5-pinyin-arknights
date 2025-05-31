ee8d0d266de2a306c91d2d9a17c7cd9116d1bfda
WantChane <wantchane@gmail.com>
2025-05-31 22:23:25 +0800

    添加肉鸽收藏品词库

---
+++ prts_collection_2.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from mw2fcitx.tweaks.moegirl import *  # type: ignore
+import os
+from custom_tweaks import *
+
+
+dict_name, _ext = os.path.splitext(os.path.basename(__file__))
+
+# region
+# 20250531220735
+# 字符 '-' (U+002D) 出现次数：56
+# 字符 '“' (U+201C) 出现次数：27
+# 字符 '”' (U+201D) 出现次数：27
+# 字符 '《' (U+300A) 出现次数：7
+# 字符 '》' (U+300B) 出现次数：7
+# 字符 '.' (U+002E) 出现次数：1
+# 字符 '·' (U+00B7) 出现次数：1
+# endregion
+
+tweaks = [
+    tweak_remove_char("“"),
+    tweak_remove_char("”"),
+    tweak_remove_char("《"),
+    tweak_remove_char("》"),
+    tweak_chinese_with(["-", "·"]),
+]
+
+
+exports = {
+    "source": {
+        "file_path": [f"input/{dict_name}_titles.txt"],
+    },
+    "tweaks": tweaks,
+    "converter": {
+        "use": "opencc",
+        "kwargs": {
+            "disable_instinct_pinyin": False,
+            "fixfile": "input/fixfile.json",
+            "characters_to_omit": ["·", "-"],
+        },
+    },
+    "generator": [
+        {
+            "use": "rime",
+            "kwargs": {
+                "name": dict_name,
+                "version": datetime.now().strftime("%Y%m%d%H%M%S"),
+                "output": f"output/{dict_name}.dict.yaml",
+            },
+        },
+        {
+            "use": "pinyin",
+            "kwargs": {"output": f"output/{dict_name}.dict"},
+        },
+    ],
+}
---
+++ script/prts_collection_1.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+dict_name, _ext = os.path.splitext(os.path.basename(__file__))
+
+url = "https://prts.wiki/api.php?action=parse&page=刻俄柏的灰蕈迷境/收藏品图鉴&format=json&formatversion=2&utf8=1"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+    soup = BeautifulSoup(data["parse"]["text"], "html.parser")
+
+    with open(f"output/{dict_name}_titles.txt", "w", encoding="utf-8") as file:
+        for table in soup.find_all("table", class_="wikitable"):
+            first_tr = table.find("tr")
+            if first_tr:
+                th_cells = first_tr.find_all("th")
+                if len(th_cells) >= 2:
+                    title = "".join(
+                        th_cells[1].find_all(string=True, recursive=False)
+                    ).strip()
+                    file.write(title + "\n")
+
+except requests.exceptions.RequestException as e:
+    print(f"请求失败：{e}")
+except Exception as e:
+    print(f"发生错误：{e}")
