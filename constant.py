b541a9ec866ca0ea4cbeec6c93d6ed6600e087f5
WantChane <wantchane@gmail.com>
2025-07-14 12:23:36 +0800

    chore(constant): update USER_AGENT value

--- constant.py
+++ constant.py
@@ -1,7 +1,7 @@
 from mw2fcitx.version import PKG_VERSION
 from datetime import datetime
 
-USER_AGENT = f"MW2Fcitx/{PKG_VERSION}; github.com/WantChane/fcitx5-pinyin-prts"
+USER_AGENT = f"MW2Fcitx/{PKG_VERSION}; github.com/WantChane/fcitx5-pinyin-arknights"
 BUILD_DATE = datetime.now().strftime("%y.%m.%d")
 REQUEST_DELAY = 2
 MW_LIMIT = "max"
