30ca5ccc5a6b9d27a8f17a66c4f5d3417d424cf0
WantChane <143467549+WantChane@users.noreply.github.com>
2025-07-21 04:23:03 +0000

    feat/incremental updates (#7)
    * refactor(script): script/extend_dictionaries.py新增命令行参数,指定词库名或者--all
    
    * refactor(script): script/build.sh新增命令行参数,指定词库名或者--all
    
    * feat(script): add log.sh
    
    * chore(script): use log.sh instead of echo
    
    * refactor(script): script/build.sh调整命令行参数-d
    
    * ci(workflow): incremental update

--- script/build.sh
+++ script/build.sh
@@ -4,18 +4,81 @@ set -euo pipefail
 
 shopt -s nullglob
 
-echo "开始处理..."
+source ./script/log.sh
+
+targets=()
+process_all=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -a|--all)
+            if [ ${#targets[@]} -gt 0 ]; then
+                error "--all 参数不能与 -d/--dictionaries 同时使用"
+                exit 1
+            fi
+            process_all=true
+            shift
+            ;;
+        -d|--dictionaries)
+            if [ "$process_all" = true ]; then
+                error "-d/--dictionaries 参数不能与 --all 同时使用"
+                exit 1
+            fi
+            IFS=',' read -r -a tmp_targets <<< "$2"
+            targets+=("${tmp_targets[@]}")
+            shift 2
+            ;;
+        *)
+            error "未知参数: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$process_all" = false ] && [ ${#targets[@]} -eq 0 ]; then
+    warn "未指定参数，默认处理所有词库"
+    process_all=true
+fi
+
+info "开始处理..."
 
 ./script/clean.sh
 
-python script/extend_dictionaries.py
+if [ "$process_all" = true ]; then
+    python script/extend_dictionaries.py --all
 
-cp -vf input/an_* output/
-cp -vf output/an_* input/
+    info "复制所有titles文件..."
+    cp -vf input/an_* output/
+    cp -vf output/an_* input/
 
-for py_file in an_*.py; do
-    echo "正在处理: ${py_file}"
-    mw2fcitx -c "${py_file}"
-done
+    for py_file in an_*.py; do
+        info "正在执行: ${py_file}"
+        mw2fcitx -c "${py_file}"
+    done
+else
+    python script/extend_dictionaries.py "${targets[@]}"
 
-echo "所有文件处理完成！"
+    for target in "${targets[@]}"; do
+    
+        if [ "$target" = "an_other" ]; then
+            info "复制手动维护的titles文件: ${target}"
+            cp -vf "input/${target}_titles.txt" "output/"
+        else
+            if [ -f "output/${target}_titles.txt" ]; then
+                info "复制自动生成的的titles文件: ${target}"
+                cp -vf "output/${target}_titles.txt" "input/"
+            else
+                warn "output/${target}_titles.txt 不存在"
+            fi
+        fi
+
+        if [ -f "${target}.py" ]; then
+            info "正在执行: ${target}.py"
+            mw2fcitx -c "${target}.py"
+        else
+            warn "${target}.py 不存在"
+        fi
+    done
+fi
+
+success "所有任务执行完成！"
--- script/clean.sh
+++ script/clean.sh
@@ -3,15 +3,17 @@
 set -e
 shopt -s extglob nullglob
 
-echo "进入目录: input"
+source ./script/log.sh
+
+info "进入目录: input"
 cd input || exit
 
 to_delete=( !(fixfile.json|an_other_titles.txt) )
 
 if [ ${#to_delete[@]} -eq 0 ]; then
-    echo "在input目录中没有可删除的文件。"
+    info "在input目录中没有可删除的文件。"
 else
-    echo "将删除input目录中的以下文件和目录："
+    warn "将删除input目录中的以下文件和目录："
     for item in "${to_delete[@]}"; do
         echo "  $item"
     done
@@ -20,17 +22,17 @@ else
     rm -rf "${to_delete[@]}"
 fi
 
-echo "input目录清理完成。"
+info "input目录清理完成。"
 
-echo "进入目录: output"
+info "进入目录: output"
 cd ../output || exit
 
 to_delete=( !(.gitkeep) )
 
 if [ ${#to_delete[@]} -eq 0 ]; then
-    echo "在output目录中没有可删除的文件。"
+    info "在output目录中没有可删除的文件。"
 else
-    echo "将删除output目录中的以下文件和目录："
+    warn "将删除output目录中的以下文件和目录："
     for item in "${to_delete[@]}"; do
         echo "  $item"
     done
@@ -38,4 +40,4 @@ else
     rm -rf "${to_delete[@]}"
 fi
 
-echo "output目录清理完成。"
+success "output目录清理完成。"
