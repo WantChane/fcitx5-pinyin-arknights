f86a8e483ab62ff1493d64379767f3fe26ac3b93
WantChane <wantchane@gmail.com>
2025-07-21 08:29:19 +0800

    chore(script): use log.sh instead of echo

--- script/build.sh
+++ script/build.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 shopt -s nullglob
 
+source ./script/log.sh
+
 targets=()
 process_all=true
 
@@ -21,19 +23,19 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo "开始处理..."
+info "开始处理..."
 
 ./script/clean.sh
 
 if [ "$process_all" = true ]; then
     python script/extend_dictionaries.py --all
 
-    echo "复制所有titles文件..."
+    info "复制所有titles文件..."
     cp -vf input/an_* output/
     cp -vf output/an_* input/
 
     for py_file in an_*.py; do
-        echo "正在执行: ${py_file}"
+        info "正在执行: ${py_file}"
         mw2fcitx -c "${py_file}"
     done
 else
@@ -42,23 +44,24 @@ else
     for target in "${targets[@]}"; do
     
         if [ "$target" = "an_other" ]; then
-            echo "复制手动维护的titles文件: ${target}"
+            info "复制手动维护的titles文件: ${target}"
             cp -vf "input/${target}_titles.txt" "output/"
         else
             if [ -f "output/${target}_titles.txt" ]; then
-                echo "复制自动生成的的titles文件: ${target}"
+                info "复制自动生成的的titles文件: ${target}"
                 cp -vf "output/${target}_titles.txt" "input/"
             else
-                echo "警告: output/${target}_titles.txt 不存在"
+                warn "output/${target}_titles.txt 不存在"
             fi
         fi
 
         if [ -f "${target}.py" ]; then
+            info "正在执行: ${target}.py"
             mw2fcitx -c "${target}.py"
         else
-            echo "警告: ${target}.py 不存在"
+            warn "${target}.py 不存在"
         fi
     done
 fi
 
-echo "所有任务执行完成！"
+success "所有任务执行完成！"
