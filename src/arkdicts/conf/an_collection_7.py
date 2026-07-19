aef595db7dd3ffbca75cd4a7b6cba38fe7185196
WantChane <143467549+WantChane@users.noreply.github.com>
2026-07-19 08:05:50 +0800

    feat: add an_collection_7

---
+++ src/arkdicts/conf/an_collection_7.py
@@ -0,0 +1,29 @@
+import os
+from arkdicts.custom_tweaks import tweak_find_chinese, tweak_remove_chars, tweak_mapping
+from arkdicts.utils.utils import generate_filepath, generate_exports
+from arkdicts.utils.parse_page import extract_text, parse_page
+
+dict_name = os.path.splitext(os.path.basename(__file__))[0]
+titles_path, rime_path, fcitx_path = generate_filepath(dict_name)
+
+parse_page(
+    page_title="沉沦者的黑流树海/拟造物质编目",
+    output_path=titles_path,
+    selector="div>table.wikitable>tbody>tr:first-child>th:nth-child(2)",
+    extractor=extract_text(recursive=False),
+)
+
+tweaks = [
+    tweak_remove_chars(["“", "”", "《", "》"]),
+    tweak_find_chinese(["-", "·"]),
+    tweak_mapping({"的狙击镜": "狙击镜"}),
+]
+
+exports = generate_exports(
+    dict_name=dict_name,
+    titles_path=titles_path,
+    rime_path=rime_path,
+    fcitx_path=fcitx_path,
+    tweaks=tweaks,
+    characters_to_omit=["-"],
+)
